e4befa563dd31c91a7df520dd432a8af9edfc797
Kyle <kyleyu23@gmail.com>
2022-03-27 02:55:06 -0500

    implement useEffect and animation on Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,43 @@
+import React, { useState, useEffect } from "react";
 import '../styles/card.css'
 
+
+const DEFAULT_CARD_CLASS = 'card';
 export default function Card(props) {
 
-    const { shuffleCards, handleCardOnClick, img, name } = props;
+    const { shuffleCards, handleCardOnClick, img, name, isSelected } = props;
 
-    const handleClick = (e) => {
-        const card = e.currentTarget.id;
-        // const cardElement = e.currentTarget;
-        handleCardOnClick(card);
+    const [cardClassList, setCardClassList] = useState(DEFAULT_CARD_CLASS);
+    const [playAnimation, setAnimation] = useState(false);
 
-        shuffleCards();
+    useEffect(() => {
+        //runs on mount and everytime the passed in [state/prop] updates
+        if (playAnimation) {
+            setTimeout(() => {
+                setCardClassList(DEFAULT_CARD_CLASS);
+                shuffleCards();
+            }, 200);
+        }
+        setAnimation(false);
+
+    }, [playAnimation])
+
+    const handleClick = () => {
+        handleCardOnClick(name);
+        handleAnimation();
+    }
+
+    function handleAnimation() {
+
+        isSelected ? setCardClassList('card lose') : setCardClassList('card win');
+
+        setAnimation(true);
     }
 
     return (
         <div
-            className="card"
-            onClick={(e) => handleClick(e)}
-            id={name}
+            className={cardClassList}
+            onClick={handleClick}
         >
             <img src={img} alt={name}></img>
 
